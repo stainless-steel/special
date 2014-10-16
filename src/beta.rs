5b443240809fd2b780f3f32ce8a557574f6a5865
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-16 16:13:58 +0200

    Moved the benchmarks from /benches to /src

--- src/beta.rs
+++ src/beta.rs
@@ -434,3 +434,36 @@ mod tests {
         }).collect(), y);
     }
 }
+
+#[cfg(test)]
+mod benches {
+    extern crate test;
+
+    use std::rand::random;
+
+    #[bench]
+    fn inc_beta(b: &mut test::Bencher) {
+        let (p, q) = (0.5, 1.5);
+        let log_beta = super::log_beta(p, q);
+        let x: Vec<f64> = range(0u, 1000).map(|_| random()).collect();
+
+        b.iter(|| {
+            for &x in x.iter() {
+                test::black_box(super::inc_beta(x, p, q, log_beta))
+            }
+        });
+    }
+
+    #[bench]
+    fn inv_inc_beta(b: &mut test::Bencher) {
+        let (p, q) = (0.5, 1.5);
+        let log_beta = super::log_beta(p, q);
+        let x: Vec<f64> = range(0u, 1000).map(|_| random()).collect();
+
+        b.iter(|| {
+            for &x in x.iter() {
+                test::black_box(super::inv_inc_beta(x, p, q, log_beta))
+            }
+        });
+    }
+}
