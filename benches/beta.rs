6cd5d4c5bc1ce811a225ccad8a463d9d1e8f50b0
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-16 13:42:18 +0200

    Renamed specfn into sfunc

--- benches/beta.rs
+++ benches/beta.rs
@@ -1,17 +1,17 @@
 extern crate test;
-extern crate specfn;
+extern crate sfunc;
 
 use std::rand::random;
 
 #[bench]
 fn inc_beta(b: &mut test::Bencher) {
     let (p, q) = (0.5, 1.5);
-    let log_beta = specfn::log_beta(p, q);
+    let log_beta = sfunc::log_beta(p, q);
     let x: Vec<f64> = range(0u, 1000).map(|_| random()).collect();
 
     b.iter(|| {
         for &x in x.iter() {
-            test::black_box(specfn::inc_beta(x, p, q, log_beta))
+            test::black_box(sfunc::inc_beta(x, p, q, log_beta))
         }
     });
 }
