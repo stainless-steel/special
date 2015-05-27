5db3414f8b3b0911a0e028e243b70fddf6906060
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-27 10:17:41 -0400

    Add a benchmark for digamma

--- benches/lib.rs
+++ benches/lib.rs
@@ -8,6 +8,17 @@ extern crate special;
 use rand::random;
 
 #[bench]
+fn digamma(b: &mut test::Bencher) {
+    let x = (0..1000).map(|_| 20.0 * random::<f64>() - 10.0).collect::<Vec<_>>();
+
+    b.iter(|| {
+        for &x in x.iter() {
+            test::black_box(special::digamma(x));
+        }
+    });
+}
+
+#[bench]
 fn inc_beta(b: &mut test::Bencher) {
     let (p, q) = (0.5, 1.5);
     let ln_beta = special::ln_beta(p, q);
