eab019a85377ab65e822d4113de2dbcad762218b
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-05-20 06:45:38 +0200

    Lay the base for generalizing the functions

--- src/lib.rs
+++ src/lib.rs
@@ -16,14 +16,46 @@ mod m;
 pub use beta::{inc_beta, inv_inc_beta, ln_beta};
 pub use gamma::{digamma, gamma, inc_gamma, ln_gamma};
 
-/// Compute the error function.
-#[inline]
-pub fn erf(x: f64) -> f64 {
-    unsafe { m::erf(x) }
+/// Special functions.
+pub trait Special {
+    /// Compute the error function.
+    fn erf(self) -> Self;
+
+    /// Compute the complementary error function.
+    fn erfc(self) -> Self;
 }
 
-/// Compute the complementary error function.
-#[inline]
-pub fn erfc(x: f64) -> f64 {
-    unsafe { m::erfc(x) }
+macro_rules! implement {
+    ($($kind:ty),*) => ($(
+        impl Special for $kind {
+            #[inline]
+            fn erf(self) -> $kind {
+                unsafe { m::erf(self as f64) as $kind }
+            }
+
+            #[inline]
+            fn erfc(self) -> $kind {
+                unsafe { m::erfc(self as f64) as $kind }
+            }
+        }
+    )*);
+}
+
+implement!(f32, f64);
+
+#[cfg(test)]
+mod tests {
+    use Special;
+
+    #[test]
+    pub fn erf() {
+        0f32.erf();
+        0f64.erf();
+    }
+
+    #[test]
+    pub fn erfc() {
+        0f32.erfc();
+        0f64.erfc();
+    }
 }
