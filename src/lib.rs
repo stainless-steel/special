df563800f14a543de6944b6bfe33d38b2a07bbfa
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-05-25 14:49:00 +0200

    Eliminate the Special trait

--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,3 @@ mod math;
 pub use beta::Beta;
 pub use error::Error;
 pub use gamma::Gamma;
-
-/// Special functions.
-pub trait Special where Self: Beta + Error + Gamma {
-}
-
-impl<T> Special for T where T: Beta + Error + Gamma {
-}
