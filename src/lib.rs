34628136e26dd9035dad5dc7a6bc8db01f219647
Baxter Eaves <baxyjr@gmail.com>
2018-10-18 18:20:48 -0500

    Add bessel i0 and i1

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,13 @@ extern crate assert;
 
 extern crate libc;
 
+mod bessel;
 mod beta;
 mod error;
 mod gamma;
 mod math;
 
+pub use bessel::Bessel;
 pub use beta::Beta;
 pub use error::Error;
 pub use gamma::Gamma;
